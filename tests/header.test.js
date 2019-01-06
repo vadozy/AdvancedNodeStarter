66448a7f9524d2ccc53c8b238e6a290f6f2048f8
Vadim Storozhuk <vadozy@yahoo.com>
2019-01-05 19:09:42 -0500

    End of lecture 79

--- tests/header.test.js
+++ tests/header.test.js
@@ -1,15 +1,34 @@
 const puppeteer = require('puppeteer');
 
+/*
 test('Adds two numbers', () => {
     const sum = 1 + 2;
     expect(sum).toEqual(3);
 });
+*/
 
-test('We can launch a browser', async () => {
-    const browser = await puppeteer.launch({
-        headless: false
+let browser, page;
+
+beforeEach(async () => {
+    browser = await puppeteer.launch({
+        headless: true
     });
-    const page = await browser.newPage();
-    await page.goto('localhost:3000');
+    page = await browser.newPage();
+    await page.goto('http://localhost:3000');
 });
 
+afterEach(async () => {
+    await browser.close();
+});
+
+test('the header has the correct text', async () => {
+    const text = await page.$eval('a.brand-logo', el => el.innerHTML);
+    expect(text).toEqual('Blogster');
+});
+
+test('clicking login starts oauth flow', async () => {
+    await page.click('.right a');
+    const url = await page.url();
+    //console.log(url);
+    expect(url).toMatch(/accounts\.google\.com/);
+});
